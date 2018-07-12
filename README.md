fd8601019a81ef6e6d8ddd846312ee048c548da6
Alister Lewis-Bowen <bowena@vmware.com>
2018-07-12 10:51:57 -0400

    Fix shar for downloaded binary

--- README.md
+++ README.md
@@ -5,5 +5,4 @@ This is a [Homebrew](https://brew.sh/) tap for formulae installing the VKE CLI o
 Assuming you have Homebrew installed, you need to tap this homebrew repo and then brew install the VKE CLI:
 
     brew tap ali5ter/homebrew-vke-cli
-    brew update
     brew install vke-cli
--- vke-cli.rb
+++ vke-cli.rb
@@ -2,7 +2,7 @@ class VkeCli < Formula
     desc "A command line tool for VMware Kuberentes Engine"
     homepage "https://cloud.vmware.com/vmware-kubernetes-engine"
     url "https://s3-us-west-2.amazonaws.com/cascade-cli-download/pre-prod-us-west-2/latest/mac/vke"
-    sha256 "90d77e8f6e540bad3ee629e844ff7876c14159ef79f8755d0392e27f02de663f"
+    sha256 "1cae4e7af7df287288b31f6efc3f494d692f144ecc582e822ad4d78d458ba9bc"
     version "0.9"
   
     bottle :unneeded
