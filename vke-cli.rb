8340d66040bdc9db5c066e29e67f80d6a718176a
Alister Lewis-Bowen <bowena@vmware.com>
2018-08-31 14:13:19 +0100

    add index to version

--- vke-cli.rb
+++ vke-cli.rb
@@ -3,7 +3,7 @@ class VkeCli < Formula
     homepage "https://cloud.vmware.com/vmware-kubernetes-engine"
     url "https://s3-us-west-2.amazonaws.com/cascade-cli-download/pre-prod-us-west-2/latest/mac/vke"
     sha256 "9419e7e64b7adecf75f2a846474b65b30da375b2e1f93977e86638bbd5dac78c"
-    version "0.9"
+    version "0.9.6"
   
     bottle :unneeded
   
