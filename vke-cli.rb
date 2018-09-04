be034e3b1c3684b430b1655fea8191d01115e6fa
Alister Lewis-Bowen <bowena@vmware.com>
2018-09-04 14:17:12 -0400

    script to check for new release and update the tap

--- vke-cli.rb
+++ vke-cli.rb
@@ -3,7 +3,7 @@ class VkeCli < Formula
     homepage "https://cloud.vmware.com/vmware-kubernetes-engine"
     url "https://s3-us-west-2.amazonaws.com/cascade-cli-download/pre-prod-us-west-2/latest/mac/vke"
     sha256 "b2f9406a19349e7e96c6090d7e064c7ece97e20237a684dbb41bd185b8b12c64"
-    version "0.9.7"
+    version "0.9.0.19-a2bb1bf"
   
     bottle :unneeded
   
