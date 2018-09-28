2416661e26e5406adfb049c61d4237e14acc4caf
Alister Lewis-Bowen <bowena@vmware.com>
2018-09-28 08:42:55 -0400

    fixes based on brew audit

--- vke-cli.rb
+++ vke-cli.rb
@@ -1,14 +1,18 @@
 class VkeCli < Formula
-    desc "A command line tool for VMware Kuberentes Engine"
-    homepage "https://cloud.vmware.com/vmware-kubernetes-engine"
-    url "https://s3-us-west-2.amazonaws.com/cascade-cli-download/pre-prod-us-west-2/latest/mac/vke"
-    sha256 "0e22c5c296b66735c601f4285f28854a62bff573a066ba1316f1fd7bb78783ae"
-    version "0.9.0.42-6ea1df5"
-    version_scheme 1
-  
-    bottle :unneeded
-  
-    def install
-      bin.install "vke"
-    end
+  desc "Command-line tool for VMware Kuberentes Engine"
+  homepage "https://cloud.vmware.com/vmware-kubernetes-engine"
+  url "https://s3-us-west-2.amazonaws.com/cascade-cli-download/pre-prod-us-west-2/latest/mac/vke"
+  version "0.9.0.42-6ea1df5"
+  version_scheme 1
+  sha256 "0e22c5c296b66735c601f4285f28854a62bff573a066ba1316f1fd7bb78783ae"
+
+  bottle :unneeded
+
+  def install
+    bin.install "vke"
   end
+
+  test do
+    system "false"
+  end
+end
