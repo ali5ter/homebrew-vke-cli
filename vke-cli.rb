23f13f9a3301c419191586d4c635cf130469dbde
CircleCI deployment job <alister@lewis-bowen.org>
2018-12-06 00:30:28 +0000

    Update tap for new build

--- vke-cli.rb
+++ vke-cli.rb
@@ -2,8 +2,8 @@ class VkeCli < Formula
   desc "Command-line tool for VMware Kuberentes Engine"
   homepage "https://cloud.vmware.com/vmware-kubernetes-engine"
   url "https://s3-us-west-2.amazonaws.com/cascade-cli-download/pre-prod-us-west-2/latest/mac/vke"
-  version "0.9.0.100-2169487"
-  sha256 "3478864b392d9f58a6c6ccc6972ef56c67bf1607710c55ff3b783e544a484b77"
+  version "0.9.0.101-62b7c81"
+  sha256 "340001decadc102748f912e5c105e799cee576b96feb5949b2ba08f239c9ee01"
   version_scheme 1
 
   bottle :unneeded
