43fc67da55129f0b26e7771b9d1f6e3047ab77b8
Alister Lewis-Bowen <bowena@vmware.com>
2019-01-08 11:21:16 -0500

    Update tap for new build

--- vke-cli.rb
+++ vke-cli.rb
@@ -3,7 +3,7 @@ class VkeCli < Formula
   homepage "https://cloud.vmware.com/vmware-kubernetes-engine"
   url "https://s3-us-west-2.amazonaws.com/cascade-cli-download/pre-prod-us-west-2/latest/mac/vke"
   version "0.9.0.108-0f0998c"
-  sha256 "4377310584ba0cc0058068d7722f6d66f6074e0f65764693ed4bd44b73622b24"
+  sha256 "4bc342cc5c0fba62750bd4c105e12774d1c0026c5eb1bd35636e40afb748d129"
   version_scheme 1
 
   bottle :unneeded
